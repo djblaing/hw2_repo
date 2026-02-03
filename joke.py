98af3c9775dd91ba84bbb354e6143141471a164b
dblaing <rqy2qb@virginia.edu>
2026-02-03 08:30:41 -0500

    update joke on trial_joke branch

--- joke.py
+++ joke.py
@@ -4,37 +4,5 @@ joke.py
 
 def joke():
 
-    print("""Source: my dad loves to retell this joke (he heard it from a late night show).
-        I found and copied a transcription someone posted on reddit :)
-        
-        A moth goes into a podiatrist’s office, and the podiatrist says, “What seems to be the problem, moth?”
-        
-        The moth says “What’s the problem? Where do I begin, man? I go to work for Gregory Illinivich,
-        and all day long I work. Honestly doc, I don’t even know what I’m doing anymore. I don’t even know
-        if Gregory Illinivich knows. He only knows that he has power over me, and that seems to bring him
-        happiness. But I don’t know, I wake up in a malaise, and I walk here and there… at night I…
-        sometimes wake up and I turn to some old lady in my bed that’s on my arm. A lady that I once loved,
-        doc. I don’t know where to turn to. My youngest, Alexendria, she fell in the… in the cold of last
-        year. The cold took her down, as it did many of us. And my other boy, and this is the hardest pill
-        to swallow, doc. My other boy, Gregarro Ivinalititavitch… I no longer love him. As much as it pains
-        me to say, when I look in his eyes, all I see is the same cowardice that I… that I catch when I take
-        a glimpse of my own face in the mirror. If only I wasn’t such a coward, then perhaps… perhaps I
-        could bring myself to… Doc, sometimes I feel like a spider, even though I’m a moth, just barely
-        hanging on to my web with an everlasting fire underneath me. I’m not feeling good.
-        
-        And so the doctor says, “Moth, man, you’re troubled. But you should be seeing a psychiatrist.
-        Why on earth did you come here?”
-          
-        PRESS ENTER FOR PUNCHLINE
-        """)
-
-    input()
-
-    print("""And the moth says, “’Cause the light was on.”
-
-        – Norm Macdonald
-        """)
-
-
     print("What do you get if you cross and elephant and a rhino?\n\n Hell if I know. \n Ha Ha Ha")
 
